58efc73a7da1d0b50a6aca46e6b8690a66ec77f1
Patyszoon <pati0755@tlen.pl>
2026-03-17 14:10:41 +0100

    Add project files.

--- src/main/java/org/example/Knight.java
+++ src/main/java/org/example/Knight.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Knight {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    // plansza rozmiarów n x n
+    public Set<Point> calculateAttack(int n, int knightX, int knightY, Set<Point> przeszkody){
+        Set<Point> possibleMoves = new HashSet<>();
+        //poziom to x, pion to y
+        int[][] ruchy={{-2,-1}, {-2,1}, {-1, -2}, {-1, 2}, {1,-2,}, {1,2}, {2,-1}, {2,1}};
+
+
+        return possibleMoves;
     }
 }
