6be0807ee91daf6a7e5587592d00fe8ea3d474fe
Kowalczuk <wkowalczuk03@gmail.com>
2026-03-24 13:58:17 +0100

    Knight na 3.0 dziala, przechodzi testy

--- src/main/java/org/example/Knight.java
+++ src/main/java/org/example/Knight.java
@@ -6,13 +6,38 @@ import java.util.Set;
 
 public class Knight {
 
-    // plansza rozmiarów n x n
+    // plansza rozmiarow n x n
     public Set<Point> calculateAttack(int n, int knightX, int knightY, Set<Point> przeszkody){
         Set<Point> possibleMoves = new HashSet<>();
-        //poziom to x, pion to y
+
+        //wszystkie mozliwe ruchy skoczka
         int[][] ruchy={{-2,-1}, {-2,1}, {-1, -2}, {-1, 2}, {1,-2,}, {1,2}, {2,-1}, {2,1}};
 
+        // sprawdzam kazdy mozliwy ruch
+        for (int[] ruch : ruchy){
+            int newX = knightX + ruch[0];
+            int newY = knightY + ruch[1];
+
+            // czy pole miesci sie na planszy
+            if(newX >= 0 && newX < n && newY >= 0 && newY < n){
+                Point nowaPozycja = new Point(newX, newY); // sprawdzana pozycja
+
+                if(!przeszkody.contains(nowaPozycja)){
+                    possibleMoves.add(nowaPozycja);
+                }
+            }else{ // jesli sie nie miesci, to odbicie
+                calculateBouncePosition(n, knightX, knightY);
+            }
+        }
 
         return possibleMoves;
+    }
+
+    // odbicie od planszy
+    public Point calculateBouncePosition(int n, int knightX, int knightY){
+        Point bouncePosition = new Point();
+        // obliczenie odbicia skoczka
+        return bouncePosition;
+
     }
 }
--- src/test/java/KnightTest.java
+++ src/test/java/KnightTest.java
@@ -1,3 +1,104 @@
+import org.example.Knight;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class KnightTest {
+
+//    // executes before each test method in this class
+//    @BeforeEach
+//    void init() {
+//        //nadac wartosci przeszkodom
+//    }
+
+    // brak przeszkod, knight znajduje sie w centrum planszy, mozliwych attakow = 8
+    @Test
+    void knightHas8PossibleMoves(){
+        Knight knight = new Knight();
+        int n = 8;
+        int knightX = 4;
+        int knightY = 3;
+        Set<Point> przeszkody = new HashSet<>();// pusty set
+
+        Set<Point> result = knight.calculateAttack(n, knightX, knightY, przeszkody);
+
+        assertThat(result, hasSize(8)); // czy sa mozliwe 8 ruchow
+        assertThat(result, hasItem(new Point(2,2)));
+        assertThat(result, hasItem(new Point(2,4)));
+        assertThat(result, hasItem(new Point(3,1)));
+        assertThat(result, hasItem(new Point(3,5)));
+        assertThat(result, hasItem(new Point(5,1)));
+        assertThat(result, hasItem(new Point(6,2)));
+        assertThat(result, hasItem(new Point(5,5)));
+        assertThat(result, hasItem(new Point(6,4)));
+    }
+
+    // skoczek w centrum, sa 2 przeszkody
+    @Test
+    void knightInCenterWith2Obstacles(){
+        Knight knight = new Knight();
+        int n = 8;
+        int knightX = 4;
+        int knightY = 3;
+        Set<Point> przeszkody = new HashSet<>();
+        przeszkody.add(new Point(3, 1));
+        przeszkody.add(new Point(3, 5));
+
+        Set<Point> result = knight.calculateAttack(n, knightX, knightY, przeszkody);
+
+        assertThat(result, hasSize(6)); // czy sa mozliwe 6 ruchow
+        assertThat(result, hasItem(new Point(2,2)));
+        assertThat(result, hasItem(new Point(2,4)));
+        assertThat(result, hasItem(new Point(5,1)));
+        assertThat(result, hasItem(new Point(6,2)));
+        assertThat(result, hasItem(new Point(5,5)));
+        assertThat(result, hasItem(new Point(6,4)));
+    }
+
+    @Test
+    void knightInCornerWith2Moves(){
+        Knight knight = new Knight();
+        int n = 8;
+        int knightX = 7;
+        int knightY = 0;
+        Set<Point> przeszkody = new HashSet<>();
+        przeszkody.add(new Point(5, 0));
+        przeszkody.add(new Point(3, 5));
+
+        Set<Point> result = knight.calculateAttack(n, knightX, knightY, przeszkody);
+
+        assertThat(result, hasSize(2)); // czy sa mozliwe 2 ruchy
+        assertThat(result, hasItem(new Point(5,1)));
+        assertThat(result, hasItem(new Point(6,2)));
+    }
+
+    @Test
+    void knightHasNoMoves(){
+        Knight knight = new Knight();
+        int n = 8;
+        int knightX = 4;
+        int knightY = 3;
+        Set<Point> przeszkody = new HashSet<>();
+        przeszkody.add(new Point(2,2));
+        przeszkody.add(new Point(2,4));
+        przeszkody.add(new Point(3,1));
+        przeszkody.add(new Point(3,5));
+        przeszkody.add(new Point(5,1));
+        przeszkody.add(new Point(6,2));
+        przeszkody.add(new Point(5,5));
+        przeszkody.add(new Point(6,4));
+
+        Set<Point> result = knight.calculateAttack(n, knightX, knightY, przeszkody);
+
+        assertThat(result, hasSize(0)); // nie ma ruchow?
+    }
+
 
 }
